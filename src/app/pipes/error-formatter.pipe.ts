5942df8e0e5af0f9c4dbeafd6104a8e6b00dca8a
NickSettler <moiseevnikita14@gmail.com>
2023-04-21 04:03:09 +0200

    fix(pipes): update error formatter pipe to show email error

--- src/app/pipes/error-formatter.pipe.ts
+++ src/app/pipes/error-formatter.pipe.ts
@@ -8,6 +8,7 @@ export class ErrorFormatterPipe implements PipeTransform {
   transform(errors: ValidationErrors | null, fieldName: string): string | null {
     if (errors?.["nonNullable"]) return `${fieldName} is required`;
     if (errors?.["required"]) return `${fieldName} is required`;
+    if (errors?.["email"]) return `${fieldName} is not valid email`;
     if (errors?.["inputMask"]) return `${fieldName} is invalid`;
 
     return null;
