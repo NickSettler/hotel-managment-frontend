67d6b03901efe0c60e044002c0a4c8d2c3015d89
NickSettler <moiseevnikita14@gmail.com>
2023-04-21 03:45:48 +0200

    feat(pipes): add error fomatting pipe

---
+++ src/app/pipes/error-formatter.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from "@angular/core";
+import { ValidationErrors } from "@angular/forms";
+
+@Pipe({
+  name: "errorFormatter",
+})
+export class ErrorFormatterPipe implements PipeTransform {
+  transform(errors: ValidationErrors | null, fieldName: string): string | null {
+    if (errors?.["nonNullable"]) return `${fieldName} is required`;
+    if (errors?.["required"]) return `${fieldName} is required`;
+    if (errors?.["inputMask"]) return `${fieldName} is invalid`;
+
+    return null;
+  }
+}
