2b7c9e202d39361b9d910c664935dbc908775f92
NickSettler <moiseevnikita14@gmail.com>
2023-04-21 03:23:10 +0200

    feat(clients): add selector to get client by passport

--- src/app/store/clients/clients.selectors.ts
+++ src/app/store/clients/clients.selectors.ts
@@ -1,4 +1,4 @@
-import { IClientsState } from "./clients.types";
+import { E_CLIENT_KEYS, IClientsState } from "./clients.types";
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 
 export const getClientsState = createFeatureSelector<IClientsState>("clients");
@@ -7,3 +7,8 @@ export const getClients = createSelector(
   getClientsState,
   (state: IClientsState) => state.clients
 );
+
+export const getClient = (passport: string) =>
+  createSelector(getClients, (clients) =>
+    clients.find((client) => client[E_CLIENT_KEYS.PASSPORT_NUMBER] === passport)
+  );
