ad310fb50ad0068466d13040f3a8420f3afcd642
NickSettler <moiseevnikita14@gmail.com>
2023-04-23 18:31:20 +0200

    feat(suites): update suites view

--- src/app/views/suites-view/suites-capacity-view/suites-capacity-view.component.ts
+++ src/app/views/suites-view/suites-capacity-view/suites-capacity-view.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
 import {
   getCapacityAction,
+  getCapacitySuccessAction,
   getSuitesAction,
 } from "../../../store/suites/suites.actions";
-import { getCapacity, getSuites } from "../../../store/suites/suites.selectors";
+import {
+  getAvailableCapacity,
+  getOccupiedCapacity,
+  getSuites,
+} from "../../../store/suites/suites.selectors";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import {
   E_SUITE_KEYS,
   E_SUITE_TYPE_KEYS,
 } from "../../../store/suites/suites.types";
+import { ActivatedRoute, Router } from "@angular/router";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
+@UntilDestroy()
 @Component({
   selector: "app-suites-capacity-view",
   templateUrl: "./suites-capacity-view.component.html",
   styleUrls: ["./suites-capacity-view.component.scss"],
 })
-export class SuitesCapacityViewComponent implements OnInit {
+export class SuitesCapacityViewComponent implements OnInit, OnDestroy {
   filterForm = new FormGroup({
     arrival: new FormControl("", {
       validators: [Validators.required],
@@ -26,29 +35,66 @@ export class SuitesCapacityViewComponent implements OnInit {
     }),
   });
 
-  constructor(private readonly store: Store) {}
+  constructor(
+    private readonly store: Store,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router
+  ) {}
 
   suites$ = this.store.select(getSuites);
 
-  capacity$ = this.store.select(getCapacity);
+  occupiedCapacity$ = this.store.select(getOccupiedCapacity);
+
+  availableCapacity$ = this.store.select(getAvailableCapacity);
 
   ngOnInit(): void {
     this.store.dispatch(getSuitesAction());
 
-    this.store.dispatch(
-      getCapacityAction({
-        arrival: "2024-01-01",
-        departure: "2024-01-02",
-        occupied: false,
-      })
-    );
+    this.route.queryParams
+      .pipe(untilDestroyed(this))
+      .subscribe((queryParams) => {
+        this.filterForm.patchValue({
+          arrival: queryParams["arrival"],
+          departure: queryParams["departure"],
+        });
+
+        this.getCapacity(null);
+      });
   }
 
-  getCapacity() {
+  getCapacity(event: Event | null) {
+    if (event) event.preventDefault();
+
     if (!this.filterForm.value.arrival || !this.filterForm.value.departure)
       return;
+
+    this.store.dispatch(
+      getCapacityAction({
+        arrival: this.filterForm.value.arrival,
+        departure: this.filterForm.value.departure,
+      })
+    );
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        arrival: this.filterForm.value.arrival,
+        departure: this.filterForm.value.departure,
+      },
+      queryParamsHandling: "merge",
+    });
   }
 
   protected readonly E_SUITE_KEYS = E_SUITE_KEYS;
   protected readonly E_SUITE_TYPE_KEYS = E_SUITE_TYPE_KEYS;
+  protected readonly faArrowUpRightFromSquare = faArrowUpRightFromSquare;
+
+  ngOnDestroy(): void {
+    this.store.dispatch(
+      getCapacitySuccessAction({
+        occupied: [],
+        available: [],
+      })
+    );
+  }
 }
