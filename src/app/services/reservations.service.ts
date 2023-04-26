ceb6ecb5e4d2d910163bed9ef7f531af3bbbc735
NickSettler <moiseevnikita14@gmail.com>
2023-04-26 17:29:42 +0200

    feat(reservations): add reservation helper class

--- src/app/services/reservations.service.ts
+++ src/app/services/reservations.service.ts
@@ -2,9 +2,25 @@ import { Injectable } from "@angular/core";
 import { ApiService } from "./api.service";
 import { Observable } from "rxjs";
 import {
+  E_RESERVATION_KEYS,
+  E_RESERVATION_STATUS,
   IReservation,
   IReservationStats,
 } from "../store/reservations/reservations.types";
+
+export class ReservationHelpers {
+  static getReservationStatus(reservation: IReservation): E_RESERVATION_STATUS {
+    const arrival = new Date(reservation[E_RESERVATION_KEYS.ARRIVAL]);
+    const departure = new Date(reservation[E_RESERVATION_KEYS.DEPARTURE]);
+    const now = new Date();
+
+    return now < arrival
+      ? E_RESERVATION_STATUS.UPCOMING
+      : now > departure
+      ? E_RESERVATION_STATUS.PAST
+      : E_RESERVATION_STATUS.CURRENT;
+  }
+}
 
 @Injectable({
   providedIn: "root",
@@ -23,7 +39,6 @@ export class ReservationsService {
 
   public getStats(): Observable<Array<IReservationStats>>;
   public getStats(year: number): Observable<Array<IReservationStats>>;
-  public getStats(year?: number): Observable<Array<IReservationStats>>;
   public getStats(year?: number): Observable<Array<IReservationStats>> {
     return this.apiService.get<Array<IReservationStats>>({
       path: `${ReservationsService.STATS_PATH}/${year || ""}`,
